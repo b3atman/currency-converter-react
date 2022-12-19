3d0d9e381a3f1c63845db3f251fcb5a4924694da
Hubert Ludwiczak <ludwiczakhubert@gmail.com>
2022-12-19 18:50:02 +0100

    adding currencies board

---
+++ src/App/currencies.js
@@ -0,0 +1,22 @@
+export const currencies = [
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.7101,
+    },
+    {
+        short: "USD",
+        name: "Dolar amerykański",
+        rate: 4.8291,
+    },
+    {
+        short: "GBP",
+        name: "Funt brytyjski",
+        rate: 5.4330,
+    },
+    {
+        short: "CHF",
+        name: "Frank szwajcarski", 
+        rate: 4.7697,
+    },
+];
