ea5c9b01d5917ae6102e1f5a80e79b0eeb3e60d4
Hubert Ludwiczak <ludwiczakhubert@gmail.com>
2023-01-01 19:25:13 +0100

    theme.js added

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+export const theme = {
+    color: {
+      smalt: "#013792",
+      silver: "#C0C0C0",
+      mercury: "#E5E5E5",
+      blueRibbon: "#025CF7",
+      white: "#fff",
+      jordyBlue: "#A1BCF8",
+      black: "#000",
+      doveGray: "#706F6F",
+    },
+};
