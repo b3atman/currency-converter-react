6f141d015370711fa51b681efb6796bb19ab4dd2
Hubert Ludwiczak <ludwiczakhubert@gmail.com>
2023-01-15 13:28:37 +0100

    deleted currencies from import and footer

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -1,24 +1,37 @@
 import { useState } from "react";
-import { currencies } from "../currencies";
 import { Result } from "./Result";
 import { Clock } from "../Clock";
-import { 
+import { useRatesData } from "./useRatesData";
+import {
   LabelText,
   Field,
   Button,
   Fieldset,
   Legend,
-  Footer,
-  Item,
+  Info,
+  Loading,
+  Failure,
 } from "./styled";
 
-export const Form = ({ calculateResult, result }) => {
-  const [currency, setCurrency] = useState(currencies[0].short);
+export const Form = () => {
+  const [result, setResult] = useState();
+  const ratesData = useRatesData();
+
+  const calculateResult = (currency, amount) => {
+    const rate = ratesData.rates[currency];
+
+    setResult({
+      sourceAmount: +amount,
+      targetAmount: amount * rate,
+      currency,
+    });
+  }
+
+  const [currency, setCurrency] = useState("EUR");
   const [amount, setAmount] = useState("");
 
   const onSubmit = (event) => {
     event.preventDefault();
-
     calculateResult(currency, amount);
   }
 
@@ -29,60 +42,69 @@ export const Form = ({ calculateResult, result }) => {
           Kalkulator walut
         </Legend>
         <Clock />
-        <p>
-          *Pola wymagane
-        </p>
-        <p>
-          <label>
-            <LabelText>
-              Kwota w PLN*:
-            </LabelText>
-            <Field
-              value={amount}
-              onChange={({ target }) => setAmount(target.value)}
-              placeholder="Wpisz kwotę w PLN"
-              type="number"
-              autoFocus
-              required
-              step="0.01"
-            />
-          </label>
-        </p>
-        <p>
-          <label>
-            <LabelText>
-              Waluta*:
-            </LabelText>
-            <Field
-              as="select"
-              value={currency}
-              onChange={({ target }) => setCurrency(target.value)}
-            >
-              {currencies.map((currency => (
-                <option
-                  key={currency.short}
-                  value={currency.short}
-                >
-                  {currency.name}
-                </option>
-              )))}
-            </Field>
-          </label>
-        </p>
-        <p>
-          <Button>Przelicz</Button>
-        </p>
-        <Result result={result} />
+        {ratesData.state === "loading"
+          ? (
+            <Loading>
+              Trwa ładowanie kursów walut...
+            </Loading>
+          )
+          : (
+            ratesData.state === "error" ? (
+              <Failure>
+                Nastąpił błąd przy pobieraniu danych...
+              </Failure>
+            ) : (
+              <>
+                <p>
+                  *Pola wymagane
+                </p>
+                <p>
+                  <label>
+                    <LabelText>
+                      Kwota w PLN*:
+                    </LabelText>
+                    <Field
+                      value={amount}
+                      onChange={({ target }) => setAmount(target.value)}
+                      placeholder="Wpisz kwotę w PLN"
+                      type="number"
+                      autoFocus
+                      required
+                      step="0.01"
+                    />
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    <LabelText>
+                      Waluta*:
+                    </LabelText>
+                    <Field
+                      as="select"
+                      value={currency}
+                      onChange={({ target }) => setCurrency(target.value)}
+                    >
+                      {Object.keys(ratesData.rates).map(((currency) => (
+                        <option
+                          key={currency}
+                          value={currency}
+                        >
+                          {currency}
+                        </option>
+                      )))}
+                    </Field>
+                  </label>
+                </p>
+                <p>
+                  <Button>Przelicz</Button>
+                </p>
+                <Result result={result} />
+                <Info>
+                  Kursy walut pobrane z Europejskiego Banku Centralnego
+                </Info>
+              </>
+            ))}
       </Fieldset>
-      <Footer>
-        Kursy walut według średniego kursu NBP z dnia 19.12.2022
-        <ul>
-          <Item>EUR - 4,6886</Item>
-          <Item>USD - 4,4153</Item>
-          <Item>GBP - 5,3926</Item>
-          <Item>CHF - 4,7453</Item>
-        </ul>
-      </Footer>
     </form>
   );
 };
