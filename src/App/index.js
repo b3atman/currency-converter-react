8a4e5e99723afd7e5d47393ffe120af555df0712
Hubert Ludwiczak <ludwiczakhubert@gmail.com>
2023-01-15 13:29:51 +0100

    cut body of App to index.js in Form

--- src/App/index.js
+++ src/App/index.js
@@ -1,30 +1,11 @@
-import { useState } from 'react';
 import { Form } from './Form';
-import { currencies } from './currencies';
 import { Wrapper } from "./styled";
 
 function App() {
 
-  const [result, setResult] = useState();
-
-  const calculateResult = (currency, amount) => {
-    const rate = currencies
-      .find(({ short }) => short === currency)
-      .rate;
-
-    setResult({
-      sourceAmount: +amount,
-      targetAmount: amount / rate,
-      currency,
-    });
-  }
-
   return (
     <Wrapper>
-      <Form
-        result={result}
-        calculateResult={calculateResult}
-      />
+      <Form />
     </Wrapper>
   );
 };
